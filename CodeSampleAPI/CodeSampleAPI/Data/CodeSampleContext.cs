7be64ebf3e487ee64949d7241ca38198cf83de5d
nvduy0511 <nvduy.k19hutech@gmail.com>
2022-06-19 22:03:05 +0700

    add trigger datetime Test

--- CodeSampleAPI/CodeSampleAPI/Data/CodeSampleContext.cs
+++ CodeSampleAPI/CodeSampleAPI/Data/CodeSampleContext.cs
@@ -43,7 +43,7 @@ namespace CodeSampleAPI.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-PDHA0NQ\\SQLEXPRESS;Database=CodeSample;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-CT0V15K\\SQLEXPRESS;Database=CodeSample;Trusted_Connection=True;");
             }
         }
 
